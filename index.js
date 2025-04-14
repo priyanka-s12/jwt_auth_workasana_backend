7e3707de9f5be147f318d5fa1e7db078f1230984
Priyanka <priyanka.ss1@outlook.com>
2025-04-14 15:26:06 +0530

    update

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
-
-app.options('*', cors()); // respond to preflight
+// app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
